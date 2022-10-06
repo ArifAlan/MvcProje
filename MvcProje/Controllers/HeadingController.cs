52ad6484062aa701a0d4df25841ed203a779ad00
ArifAlan <arifalan001@gmail.com>
2022-10-07 00:03:58 +0300

    Mvc

--- MvcProje/Controllers/HeadingController.cs
+++ MvcProje/Controllers/HeadingController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +12,30 @@ namespace MvcProje.Controllers
     public class HeadingController : Controller
     {
         HeadingManager headingManager = new HeadingManager(new EfHeadingDal());
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
         public ActionResult Index()
         {
             var headingValues = headingManager.GetAll();
             return View(headingValues);
+        }
+        [HttpGet]
+        public ActionResult AddHeading()
+        {
+            List<SelectListItem> valueCategory = (from x in categoryManager.GetAll()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value=x.CategoryId.ToString()
+                                                  }).ToList();
+            ViewBag.valuecategory = valueCategory;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddHeading(Heading heading)
+        {
+            heading.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            headingManager.Add(heading);
+            return RedirectToAction("Index");
         }
     }
 }
